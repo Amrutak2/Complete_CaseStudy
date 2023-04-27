214789e0db7eeec3e034e1c3b2d499350ce844be
Kulkarni <amruta-milind.kulkarni@capgemini.com>
2023-04-27 11:25:14 +0530

    Complete Case study

---
+++ RollOff_Test4API/Models/DTO/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RollOff_Test4API.Models.DTO
+{
+    public class User
+    {
+       [Key]
+       public int Id {get; set;}
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ RollOff_Test4API/Models/Domain/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RollOff_Test4API.Models.Domain
+{
+    public class Login
+    {
+        [Key]
+        public int Id {get;set;}
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+    }
+}
